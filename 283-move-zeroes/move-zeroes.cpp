2ac570e014c94dfe5f8879eea9d4d4872db177c0
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-07-17 00:25:51 +0530

    Time: 4 ms (10.68%) | Memory: 25.1 MB (5.07%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,18 +3,18 @@ public:
     void moveZeroes(vector<int>& v) {
         int n=v.size();
         int count=0;
+        vector<int>r;
 for(int i=0;i<n;i++){
     if(v[i]==0){
         count++;
     }
-}
-for(int i=0;i<n-count;i++){
-    if(v[i]==0){
-         v.erase(v.begin()+i);//O(n)
-          v.emplace_back(0);
-          i--;
+    else{
+        r.push_back(v[i]);
     }
 }
-
+while(count--){
+    r.push_back(0);
+}
+v.assign(r.begin(),r.end());
     }
 };
