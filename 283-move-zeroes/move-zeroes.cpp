63533f2d532d84d8a02b095a36c2462f9d30e055
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-07-19 07:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (8.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,21 +5,17 @@ public:
         if(n==1){
             return;
         }
-        int j=-1;
-        for(int i=0;i<n;i++){
-            if(v[i]==0){
-                j=i;
-                break;
-            }
-        }
-        if(j==-1){
-            return;
-        }
-        for(int i=j+1;i<n;i++){
-            if(v[i]!=0){
-                swap(v[i],v[j]);
-                j++;
-            }
-        }
+        for(int i=0,j=1;j<n;j++){
+    if(v[i]!=0){
+        i++;
+    }
+    else if(v[i]==0&&v[j]!=0){
+        swap(v[i],v[j]);
+        i++;
+    }
+}
+
+  
+
     }
 };
