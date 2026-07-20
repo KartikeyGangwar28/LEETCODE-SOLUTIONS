f78825781cabe2c6f209be2f10e1ebd47919baef
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-07-20 22:01:18 +0530

    Time: 14 ms (90.73%) | Memory: 60.3 MB (84.91%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,11 +3,8 @@ public:
     int longestConsecutive(vector<int>& v) {
         int n=v.size();
         sort(v.begin(),v.end());
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
+        if(n<=1){
+            return n;
         }
         int count=1,m=1;
         for(int i=0;i<n-1;i++){
