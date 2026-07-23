7af9abc477384be7f71e17dd34d37f11487ef221
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-07-23 19:39:22 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (81.47%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& v) {
-        int n=v.size();
-        if(n==1){
+    int maxSubArray(vector<int>& v) { //O(N) not optimal means we have to do logn;
+        int n = v.size();
+        if (n == 1) {
             return v[0];
         }
-        int sum=0,m=INT_MIN;
-        for(int i=0;i<n;i++){
-            sum+=v[i];
-            m=max(sum,m);
-            if(sum<0){
-                sum=0;
+        int sum = 0, m = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            sum += v[i];
+            m = max(sum, m);
+            if (sum < 0) {
+                sum = 0;
             }
         }
         return m;
