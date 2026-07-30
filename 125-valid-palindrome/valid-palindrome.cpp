72516529cd43141bec27d647955d0e4bef0fdf2e
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-07-30 22:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (37.07%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n=s.size();
-         string r;
-         for(int i=0;i<n;i++){
-            if(isalpha(s[i])){
-                r.push_back(tolower(s[i]));
-            }
-           else if(isalnum(s[i])){
-                r.push_back(s[i]);
-            }
-         }
-         int l=r.size();
-         for(int i=0,j=l-1;i<j;i++,j--){
-            if(r[i]!=r[j]){
-                return false;
-            }
-         }
-        return true;
-    //     int l=0,r=n-1;
-    //    while(l<r){
-    //         if(isalpha(s[l])&&!isalpha(s[r])){
-    //                 r--;
-    //         }
-    //         else if(!isalpha(s[l])&&isalpha(s[r])){
-    //             l++;
-    //         }
-    //         else if(isalpha(s[l])&&isalpha(s[r])){
-    //             // s[l]=tolower(s[l]);
-    //             // s[r]=tolower(s[r]);
-    //             if(tolower(s[l])!=tolower(s[r])){
-    //                 return false;
-    //             }
-    //             l++;
-    //             r--;
-    //         }
-    //         else if(isalnum(s[l])&&isalnum(s[r])){
-    //             if(s[r]!=s[l]){
-    //                 return false;
-    //             }
-    //             l++;
-    //             r--;
-    //         }
-    //         else if(isalnum(s[l])&&!isalnum(s[r])){
-    //             l++;
-    //         }
-    //         else if(!isalnum(s[l])&&isalnum(s[r])){
-    //             r--;
-    //         }
-    //    }
-        
-    //     return false;
+         int n=s.size();
+        int l=0,r=n-1;
+  while(l<r){
+    if(isspace(s[l])){
+        l++;
+    }
+    else if(isspace(s[r])){
+        r--;
+    }
+    else if(ispunct(s[l])){
+        l++;
+    }
+    else if(ispunct(s[r])){
+        r--;
+    }
+    else if(isalpha(s[l])&&isalpha(s[r])){
+        if(tolower(s[l])!=tolower(s[r])){
+               return false;
+        }
+        l++;
+        r--;
+       }
+       else if(isalnum(s[l])&&isalnum(s[r])){
+        if(s[l]!=s[r]){
+            return false;
+        }
+        l++;
+        r--;
+       }
+        // if(!isalpha(s[l])&&!isalpha(s[r])&&!isalnum(s[r])&&!isalnum(s[l]))
+        else if(isalpha(s[l])&&isalnum(s[r])||isalpha(s[r])&&isalnum(s[l])){
+            return false;
+        }
+       else {
+        l++;
+        r--;
+       }
+   } 
+   return true;
     }
 };
