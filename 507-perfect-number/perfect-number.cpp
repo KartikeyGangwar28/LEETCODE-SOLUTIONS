4c6102a403c7bdbb50c1592a4528d77caee79761
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-07-30 16:27:47 +0530

    Time: 1037 ms (53.98%) | Memory: 7.9 MB (22.32%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -4,15 +4,12 @@ public:
         if(num<=5){
             return false;
         }
-        int sum=0;
-        for(int i=1;i<=num/2;i++){
+        int sum=1;
+        for(int i=2;i<=num/2;i++){
             if(num%i==0){
                 sum+=i;
             }
         }
-        if(sum==num){
-            return true;
-        }
-        return false;
+        return sum==num;
     }
 };
