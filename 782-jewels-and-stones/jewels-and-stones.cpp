186ea75926317e9b5561889b7b2cf7c597da37b0
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-02 19:30:55 +0530

    Time: 3 ms (11.54%) | Memory: 8.7 MB (25.72%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n=jewels.size();
+        unordered_map<char,int>m;
+        for(char c:stones){
+            m[c]++;
+        }
+         int count=0;
+            for(int i=0;i<n;i++){
+                if(m.find(jewels[i])!=m.end()){
+                    count+=m[jewels[i]];
+                }
+        }
+         return count;
+     
+     
+    }
+};
