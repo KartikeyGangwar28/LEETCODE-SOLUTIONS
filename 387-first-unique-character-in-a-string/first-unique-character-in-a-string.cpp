bb50a749c36464633f8c2ac164027c181442e0f8
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-03 22:31:09 +0530

    Time: 11 ms (49.54%) | Memory: 14.8 MB (56.37%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int firstUniqChar(string s) {
                 int n=s.size();
-                string notValid;
-          for(int i=0;i<n;i++){
-               if(s.find(s[i],i+1)!=string::npos){
-                   notValid.push_back(s[i]);
-
-               }
-               else if(notValid.find(s[i])==string::npos){return i;
-               }
-          }
-          return -1;
+        unordered_map<char,int>m;
+        for(char c:s){
+            m[c]++;
+        }
+        char ele;
+        for(int i=0;i<n;i++){
+            if(m[s[i]]==1){
+                return i;
+            }
+        }
        
+       return -1;
     }
 };
