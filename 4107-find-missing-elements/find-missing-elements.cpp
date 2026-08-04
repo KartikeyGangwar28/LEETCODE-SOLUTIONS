3397ac216371ec2717b4224754a7a3d594aba4a5
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-04 09:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 33.6 MB (28.39%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -6,8 +6,14 @@ public:
         vector<int>ans;
         unordered_set<int>s(nums.begin(),nums.end());
         int n=nums.size();
+        // for(int i=mi;i<ma;i++){
+        //     if(s.find(i)==s.end()){
+        //         ans.push_back(i);
+        //     }
+        // }
         for(int i=mi;i<ma;i++){
-            if(s.find(i)==s.end()){
+            auto it=find(nums.begin(),nums.end(),i);
+            if(it==nums.end()){
                 ans.push_back(i);
             }
         }
