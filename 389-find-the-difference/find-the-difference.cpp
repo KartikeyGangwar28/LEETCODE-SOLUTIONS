b32a02909b0f36df7f81a01087823fe294609257
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-04 17:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.95%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -3,19 +3,27 @@ public:
     char findTheDifference(string s, string t) {
      int n=s.size();
      int m=t.size();
-     unordered_map<char,int>first;
-          unordered_map<char,int>second;
-          for(auto c:s){
-            first[c]++;
-          }
-          for(auto c:t){
-            second[c]++;
-          }
-          for(int i=0;i<m;i++){
-            if(first[t[i]]!=second[t[i]]){
-                return t[i];
-            }
-          }
-          return t[n-1];
+    //  unordered_map<char,int>first;
+    //       unordered_map<char,int>second;
+    //       for(auto c:s){
+    //         first[c]++;
+    //       }
+    //       for(auto c:t){
+    //         second[c]++;
+    //       }
+    //       for(int i=0;i<m;i++){
+    //         if(first[t[i]]!=second[t[i]]){
+    //             return t[i];
+    //         }
+    //       }
+    //       return t[n-1];
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    for(int l=0;l<n;l++){
+        if(s[l]!=t[l]){
+            return t[l];
+        }
+    }
+    return t[m-1];
     }
 };
