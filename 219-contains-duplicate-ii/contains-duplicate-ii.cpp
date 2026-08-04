7206b11c0afc2115bda5dffd298e880d1621a96d
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-04 18:43:24 +0530

    Time: 95 ms (38.43%) | Memory: 125 MB (11.42%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) { 
+        int n=nums.size(); 
+      //can be done by 2 pointer as well
+      unordered_map<int,int>m;
+      for(int i=0;i<n;i++){
+        if(m.count(nums[i])&&i-m[nums[i]]<=k){
+            return true;
+        }
+        m[nums[i]]=i;
+      }
+      return false;
+    }
+};
