9fcbaa90a669247d493d24be61e0e7efdd80579b
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-08 21:08:47 +0530

    Time: 4 ms (33.38%) | Memory: 28.2 MB (67.52%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>m;
+        for(int x:nums){
+            if(x%2==0){
+            m[x]++;
+            }
+        }
+    int ans=-1;
+        for(int i=0;i<n;i++){
+            if(m.find(nums[i])!=m.end()){
+                if(m[nums[i]]==1){
+                    return nums[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
