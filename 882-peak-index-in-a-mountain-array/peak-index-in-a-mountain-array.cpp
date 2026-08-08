64c480e92c08ab4b3691ab88606268b1438152ec
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-08 23:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (98.55%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -5,14 +5,15 @@ public:
         int low=0,high=n-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(mid!=0&&mid!=n-1&&v[mid]>v[mid-1]&&v[mid]>v[mid+1]){
-                return mid;
+        //     if(mid!=0&&mid!=n-1&&v[mid+1]<v[mid]&&v[mid]>v[mid-1]){
+        //         return mid;
+        //     }
+        //   else
+         if(v[mid+1]<v[mid]&&mid!=0){
+                high=mid-1;
             }
             else if(v[mid+1]>v[mid]){
                 low=mid+1;
-            }
-            else{
-                high=mid-1;
             }
         }
         return low;
