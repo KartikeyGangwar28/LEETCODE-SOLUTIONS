8bf1d876cf80776d605931b50d61be7be1000a50
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-08 21:09:54 +0530

    Time: 4 ms (33.38%) | Memory: 28.3 MB (60.53%) - LeetSync

--- 4252-first-unique-even-element/first-unique-even-element.cpp
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -10,10 +10,13 @@ public:
         }
     int ans=-1;
         for(int i=0;i<n;i++){
-            if(m.find(nums[i])!=m.end()){
-                if(m[nums[i]]==1){
-                    return nums[i];
-                }
+            // if(m.find(nums[i])!=m.end()){
+            //     if(m[nums[i]]==1){
+            //         return nums[i];
+            //     }
+            // }
+            if(m[nums[i]]==1){
+                return nums[i];
             }
         }
         return ans;
