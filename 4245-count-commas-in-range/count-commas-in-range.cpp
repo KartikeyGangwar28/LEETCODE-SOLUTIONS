e6a57ec5d8d22c7c7e046a3bcaaf1125181368cd
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-08 21:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (51.28%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -4,12 +4,13 @@ public:
         if(n<1000){
             return 0;
         }
-       int ans=1;
-       int temp=1000;
-       while(temp!=n){
-        ans++;
-        temp++;
-       }
-       return ans;
+    //    int ans=1;
+    //    int temp=1000;
+    //    while(temp!=n){
+    //     ans++;
+    //     temp++;
+    //    }
+    //    return ans;
+    return n-999;
     }
 };
