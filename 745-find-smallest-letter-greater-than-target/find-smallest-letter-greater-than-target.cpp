0881c63c92d786d010b86f22f7d6092f6fc620d6
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-08 19:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (24.07%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int n=letters.size();
-        if(target<letters[0]||target>letters[n-1]){
+        if(target<letters[0]||target>=letters[n-1]){
             return letters[0];
         }
         int low=0,high=n-1,mid;
         while(low<=high){
             mid=low+(high-low)/2;
             if(letters[mid]==target){
-                if(mid==n-1){
-                    return letters[0];
-                }
+                // if(mid==n-1){
+                //     return letters[0];
+                // }
                 // return letters[mid+1];
                 low=mid+1;
             }
