7d41e3c2314eb18f6f993dcb8185ee6595d070fa
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-12 16:50:31 +0530

    Time: 55 ms (27.32%) | Memory: 67 MB (24.92%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>ans;
-        unordered_map<int,int>m;
-        int n=nums.size();
-            if(n<=1){
-                return ans;
-            }
-        for(int x:nums)m[x]++;    
-        
-        for(auto&it:m){
-            if(it.second>1){
-                ans.push_back(it.first);
-            }
-        }
+      int n=nums.size();
+      if(n<=1){
         return ans;
+      }
+      unordered_map<int,int>x;
+      for(int i=0;i<n;i++){ 
+                        x[nums[i]]++;
+                 if(x[nums[i]]>1){
+                    ans.push_back(nums[i]);
+                 }
+                 
+                 
+      }
+      return ans;
     }
 };
