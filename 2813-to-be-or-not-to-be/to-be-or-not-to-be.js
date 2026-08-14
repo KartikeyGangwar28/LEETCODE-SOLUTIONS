047dab505ed261af537b45ece2db9e9541cafdb6
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-14 21:38:47 +0530

    Time: 53 ms (9.81%) | Memory: 52.8 MB (90.61%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe:function(value){
+            if(value===val)return true;
+            else throw new Error("Not Equal");
+
+        },
+        notToBe:function(value){
+            if(value===val)throw new Error("Equal");
+            else return true;
+
+        }
+    }
+       
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
