274a783d4de853b5529f09d78468940c2aed8389
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-21 19:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (1.86%) - LeetSync

---
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+             sum+=nums[i];
+        }
+        int count=0;
+       while(sum%k!=0){
+        sum-=1;
+        count++;
+       }
+       return count;
+    }
+};
