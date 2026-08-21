676edda7e9d2e9270423dfacca08be8becc2e8f1
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-21 19:53:09 +0530

    Time: 4 ms (80.60%) | Memory: 133.5 MB (37.49%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+       int n=nums.size();
+        vector<int>ans;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==pivot){
+                count++;
+                continue;
+            }
+           else if(nums[i]<pivot){
+                ans.push_back(nums[i]);
+            }
+        }
+        while(count--){
+            ans.push_back(pivot);
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+
+    }
+};
