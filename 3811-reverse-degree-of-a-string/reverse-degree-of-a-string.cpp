e1c865f2a898eb5b92231d1147e6630d344362b1
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-22 09:31:39 +0530

    Time: 3 ms (17.67%) | Memory: 9.8 MB (15.34%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int prod=(i+1)*(123-s[i]);
+            sum+=prod;
+        }
+        return sum;
+    }
+};
