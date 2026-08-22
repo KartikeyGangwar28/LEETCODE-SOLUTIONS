00a3aaa810abdb4d454c8e3fc6df8dc95d5f2ddc
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-22 09:47:20 +0530

    Time: 2 ms (19.65%) | Memory: 28.3 MB (62.49%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -10,7 +10,24 @@ public:
                 nums[i]=1;
             }
         }
-        sort(nums.begin(),nums.end());
+      int z=0,o=0;
+      for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            z++;
+        }
+        else{
+            o++;
+        }
+      }
+      int i=0;
+      while(z--){
+        nums[i]=0;
+        i++;
+      }
+      while(o--){
+        nums[i]=1;
+        i++;
+      }
         return nums;
     }
 };
