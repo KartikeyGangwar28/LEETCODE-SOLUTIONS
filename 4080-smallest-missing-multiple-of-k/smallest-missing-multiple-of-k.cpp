9a516b53c9f34da8376c4550b074af89a60d7578
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-25 11:07:26 +0530

    Time: 3 ms (38.06%) | Memory: 25.1 MB (24.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        vector<int>v(10000);
+        // vector<int>v(10000);
+        unordered_map<int,int>mp;
+        for(int x:nums){
+            mp[x]++;
+        }
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]%k==0){
-                v[nums[i]]++;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]%k==0){
+        //         v[nums[i]]++;
+        //     }
+        // }
+        // int ans=0;
+        // for(int i=k;i<v.size();i+=k){
+        //     if(v[i]==0){
+        //         ans=i;
+        //         break;
+        //     }
+        // }
+        // return ans;
+        for(int i=1;i<=100;i++){
+            int tocheck=k*i;
+            if(mp.count(tocheck)==0){
+                return tocheck;
             }
         }
-        int ans=0;
-        for(int i=k;i<v.size();i+=k){
-            if(v[i]==0){
-                ans=i;
-                break;
-            }
-        }
-        return ans;
+        return k*101;
     }
 };
