6124299960218591948d9c0b92df0c00f66d29a1
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-25 16:37:15 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (22.21%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -3,13 +3,18 @@ public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans;
+        int x=ans.size();
         unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             if(mp[nums[i]]>0){
                 ans.push_back(nums[i]);
+                x++;
             }
             else{
                 mp[nums[i]]++;
+            }
+            if(x==2){
+                return ans;
             }
         }
         return ans;
