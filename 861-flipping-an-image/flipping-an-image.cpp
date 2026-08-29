d56027a30d33fb855b35888dd1ce812ac4b0f7a0
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-29 13:11:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (69.19%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.cpp
+++ 861-flipping-an-image/flipping-an-image.cpp
@@ -2,21 +2,28 @@ class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
       int n=image.size();
-      for(int i=0;i<n;i++){
-        for(int j=0,k=n-1;j<k;j++,k--){
+      for(int i=0;i<n;i++){//try to do both modification in same array next time;
+        for(int j=0,k=n-1;j<=k;j++,k--){
             swap(image[i][j],image[i][k]);
-        }
-      }
-      for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(image[i][j]==0){
-                image[i][j]=1;
+            if(j==k){
+                image[i][j]=!image[i][j];
             }
             else{
-                image[i][j]=0;
+            image[i][j]=!image[i][j];
+            image[i][k]=!image[i][k];
             }
         }
       }
+    //   for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(image[i][j]==0){
+    //             image[i][j]=1;
+    //         }
+    //         else{
+    //             image[i][j]=0;
+    //         }
+    //     }
+    //   }
       return image;
     }
 };
