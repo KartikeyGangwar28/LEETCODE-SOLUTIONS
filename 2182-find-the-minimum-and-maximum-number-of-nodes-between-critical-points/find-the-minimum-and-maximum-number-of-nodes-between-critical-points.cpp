6faadf620dd7e4f4db0726f8c2226d15250d4f8a
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-31 14:48:14 +0530

    Time: 1 ms (75.76%) | Memory: 128.6 MB (17.95%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -19,46 +19,78 @@ int length(ListNode*head){
     }
     return l;
 }
-    vector<int> nodesBetweenCriticalPoints(ListNode* head) {//trace the difference bw two adjacent lasts as well;
-     int l=length(head);
+    // vector<int> nodesBetweenCriticalPoints(ListNode* head) {//trace the difference bw two adjacent lasts as well;
+    //  int l=length(head);
+    //  if(l<=2)return{-1,-1};
+    //  ListNode*prev=head;
+    //  ListNode*curr=prev->next;
+    //  ListNode*next=curr->next; 
+    //  int first=INT_MAX,last=INT_MIN,count=1,mn=INT_MAX,mx=INT_MIN;
+    //  while(next){
+    //     if(curr->val>prev->val&&(curr->val>next->val)){
+    //      //   int test=first;
+    //          first=min(first,count);
+    //          int prev=last;
+    //          last=max(last,count);
+    //          if(first!=last){
+    //            mn=min({mn,abs(first-last),abs(prev-last)});
+    //           }
+    //         mx=max(mx,abs(first-last));
+             
+    //     }
+    //     else if(curr->val<prev->val&&(curr->val<next->val)){
+    //         first=min(first,count);
+    //         int prev=last;
+    //         last=max(last,count);
+    //         if(first!=last){
+    //            mn=min({mn,abs(first-last),abs(prev-last)});
+    //            }
+    //         mx=max(mx,abs(first-last));
+            
+    //     }
+    //     count++;
+    //    prev=curr;
+    //    curr=next;
+    //    next=next->next;
+    //  }  
+    // //  if(mn==0){
+    // //     mn=mx;
+    // //  }
+    // if(mn==INT_MAX||mx==INT_MIN){
+    //     return {-1,-1};
+    // }
+    //  return {mn,mx};
+    // }
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+         int l=length(head);
      if(l<=2)return{-1,-1};
      ListNode*prev=head;
-     ListNode*curr=prev->next;
+     vector<int>store;
+      ListNode*curr=prev->next;
      ListNode*next=curr->next; 
-     int first=INT_MAX,last=INT_MIN,count=1,mn=INT_MAX,mx=INT_MIN;
-     while(next){
+     int count=0;
+      while(next){
         if(curr->val>prev->val&&(curr->val>next->val)){
-         //   int test=first;
-             first=min(first,count);
-             int prev=last;
-             last=max(last,count);
-             if(first!=last){
-               mn=min({mn,abs(first-last),abs(prev-last)});
-              }
-            mx=max(mx,abs(first-last));
+             store.push_back(count);
              
         }
         else if(curr->val<prev->val&&(curr->val<next->val)){
-            first=min(first,count);
-            int prev=last;
-            last=max(last,count);
-            if(first!=last){
-               mn=min({mn,abs(first-last),abs(prev-last)});
-               }
-            mx=max(mx,abs(first-last));
-            
-        }
+           store.push_back(count);  
+        } 
         count++;
        prev=curr;
        curr=next;
        next=next->next;
      }  
-    //  if(mn==0){
-    //     mn=mx;
-    //  }
-    if(mn==INT_MAX||mx==INT_MIN){
-        return {-1,-1};
-    }
-     return {mn,mx};
+   int n=store.size();
+   if(n<=1){
+    return {-1,-1};
+   }
+   int mx=store[n-1]-store[0];
+   int mn=INT_MAX;
+   for(int i=0;i<n-1;i++){
+     mn=min(mn,store[i+1]-store[i]);
+   }
+   return {mn,mx};
     }
 };
