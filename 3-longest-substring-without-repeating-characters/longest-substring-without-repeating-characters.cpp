2408abfbc4d1d5a6bd596967480c78f59489a98e
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-31 23:35:18 +0530

    Time: 741 ms (5.04%) | Memory: 381.9 MB (5.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size(),count=0,as=0;
-          string ans;
-          for(int i=0;i<n;i++){ 
-            string ans;count=0;
-           for(int j=i;j<n;j++){
-                  if(ans.size()==0||(ans.find(s[j])==string::npos)){
-                        ans.push_back(s[j]);
-                        count+=1; 
-                       
-                  }
-                  else if(ans.find(s[j])!=string::npos){
-                    break;
-                  }
-           }  
-           as=max(as,count);
-            
-          }
-          return as;
+        int n=s.size(),ans=0;
+        if(n<=1){
+            return n;
+        }
+        for(int i=0;i<n;i++){
+            vector<int>hash(256,0);
+            int count=0;
+            for(int j=i;j<n;j++){
+                  if(hash[s[j]]==1)break;
+                  hash[s[j]]++;
+                  count++;
+            }
+            ans=max(ans,count);
+        }
+        return ans;
+   
     }
 };
