0c0ac2a746a92635c492f5ddb9b3c46050f183ae
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-08-31 13:16:31 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (76.41%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n=grid.size();
-        unordered_map<int,int>mp;
-        for(auto& x:grid)
-            for(int m:x)mp[m]++;
-        int doub=-1,miss=-1;
-    for(int i=1;i<=n*n;i++){
-        if(doub!=-1&&miss!=-1){
-            return {doub,miss};
+        vector<int>hash((n*n)+1);
+        // int actual=(n*(n+1))/2;
+        // int sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                // sum+=grid[i][j];
+                hash[grid[i][j]]++;
+            }
         }
-        if(mp[i]==2)doub=i;
-        if(mp[i]==0)miss=i;
-    }
-    return {doub,miss};
+        int missing=0,twice=0;
+        for(int i=1;i<=n*n;i++){
+            if(hash[i]==0){
+                missing=i;
+            }
+            if(hash[i]==2){
+                twice=i;
+            }
+        }
+        return {twice,missing};
     }
 };
