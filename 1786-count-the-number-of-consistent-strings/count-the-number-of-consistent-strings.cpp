3dc4541c8738cc64336d109d28d271d42f1e64d2
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-09-01 16:22:01 +0530

    Time: 39 ms (84.34%) | Memory: 33.9 MB (60.77%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-bool checkConsistent(string s,string allowed){
-unordered_map<char,int>mp;
-for(char c:allowed)mp[c]++;
-int n=s.size();
-for(int i=0;i<n;i++){
-    if(mp[s[i]]==0){
-        return false;
-    }
-    // mp[s[i]]--;
-}
-return true;
-}
+// bool checkConsistent(string s,string allowed){
+// unordered_map<char,int>mp;
+// for(char c:allowed)mp[c]++;
+// int n=s.size();
+// for(int i=0;i<n;i++){
+//     if(mp[s[i]]==0){
+//         return false;
+//     }
+//     // mp[s[i]]--;
+// }
+// return true;
+// }
     int countConsistentStrings(string allowed, vector<string>& words) {
         int n=words.size();
-        int x=allowed.size();
+       unordered_map<char,int>mp;
+        for(char c:allowed)mp[c]++;
         int count=0;
         for(int i=0;i<n;i++){
-               if(checkConsistent(words[i],allowed))count+=1;
+            bool flag=true;
+               for(int j=0;j<words[i].size();j++){
+                if(mp[words[i][j]]==0){
+                    flag=false;
+                    break;
+                }
+               }
+               if(flag)count++;
         }
         return count;
     }
