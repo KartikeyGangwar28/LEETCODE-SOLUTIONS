75373b49ef8eeb03adf9f9f7d74280af6e37c738
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-09-05 20:02:59 +0530

    Time: 6 ms (80.60%) | Memory: 210.9 MB (15.41%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+     int n=nums.size();
+     if(n==1){
+       return 0;
+        //return -1;
+     }
+     
+    vector<int>pre(n);
+    vector<int>suff(n);
+    pre[0]=nums[0];
+    suff[n-1]=nums[n-1];
+    for(int i=1;i<n;i++){
+        pre[i]=max(pre[i-1],nums[i]);
+    }
+    for(int i=n-2;i>=0;i--){
+        suff[i]=min(suff[i+1],nums[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(pre[i]-suff[i]<=k)return i;
+    }
+    return -1;
+    }
+};
