6e8fe4c6059b87fe3e87258e3e30283926dc49a7
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-09-07 13:37:35 +0530

    Time: 2 ms (10.07%) | Memory: 11.6 MB (99.70%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+        if(n<=1)return strs[0];
+        string s=strs[0];
+        int count=0,ans=INT_MAX;
+        for(int i=1;i<n;i++){
+            count=0;
+            for(int j=0;j<strs[i].size();j++){
+                if(strs[i][j]==s[j])count++;
+                else break;
+            }
+            ans=min(ans,count);
+        }
+       // cout<<ans;
+    string as;
+    for(int i=0;i<ans;i++){
+  as.push_back(s[i]);
+    }
+    return as;
+
+    }
+};
