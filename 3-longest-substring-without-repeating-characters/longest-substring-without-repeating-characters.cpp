16ab04bed92a1908f40678af288a8f56e78d995b
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-09-09 22:30:59 +0530

    Time: 83 ms (30.38%) | Memory: 19.3 MB (34.62%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size(),ans=0;
-        if(n<=1){
-            return n;
-        }
-        int l=0,r=0;
-        vector<int>hash(256,0);
-        int count=0;
-        while(r<n){ 
-        ans=max(ans,r-l+1);
-        hash[s[r]]++;
-           r++;
-           while(hash[s[r]]>0){
-            hash[s[l]]--;
-            l++;
-           }
-          
-           }
-        
-        return ans;
-   
+        int n=s.size(),low=0,high=0;
+        unordered_map<char,int>mp;
+        int count=0,ans=INT_MIN;
+        while(high<n){
+            while(mp[s[high]]!=0){
+                mp[s[low]]--;
+                 low++;
+                count--;
+            }
+         if(mp[s[high]]==0){
+                mp[s[high]]++;
+                high++;
+                count++;
+            }
+            //cout<<ans<<" "<<count<<"\n";
+            ans=max(ans,count);
+        }  
+   return max(0,ans);
     }
 };
