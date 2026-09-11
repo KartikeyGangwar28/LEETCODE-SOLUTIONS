5fa22da91696c8e6f2e863c6b9dc454eba3d297b
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-09-11 21:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (31.47%) - LeetSync

---
+++ 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getLeastFrequentDigit(int n) {
+        unordered_map<int,int>mpp;
+        int ans=INT_MAX;
+        while(n!=0){
+            mpp[n%10]++;
+            n/=10;
+        }
+        int least=INT_MAX;
+        for(auto&it:mpp){
+             least=min(least,it.second);
+        }
+        for(auto&it:mpp){
+            if(it.second==least){
+                ans=min(ans,it.first);
+            }
+        }
+        return ans;
+    }
+};
