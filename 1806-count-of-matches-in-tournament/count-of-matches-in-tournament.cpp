cee45797f102d013961115a237cdc1a53b32d01a
Kartikey Gangwar <WORK.KARTIKEYGANGWAR@GMAIL.COM>
2026-09-18 00:48:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (34.26%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-int matches=0;
+//int matches=0;
     int numberOfMatches(int n) {
-        while(n!=1){
-            if(n%2==0){
-                matches+=n/2;
-                n=n/2;
-            }
-            else{
-                matches+=(n-1)/2;
-                n=((n-1)/2)+1;
-            }
-        }
-        return matches;
+        // while(n!=1){
+        //     if(n%2==0){
+        //         matches+=n/2;
+        //         n=n/2;
+        //     }
+        //     else{
+        //         matches+=(n-1)/2;
+        //         n=((n-1)/2)+1;
+        //     }
+        // }
+        // return matches;
+        return n-1;
     }
 };
